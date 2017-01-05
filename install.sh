87f84debe21a05775564cf05718d947e8769777b
Yousheng Ji <jasonyoushengji@gmail.com>
2017-01-05 20:57:52 +0800

    add zsh config

---
+++ install.sh
@@ -0,0 +1,10 @@
+main() {
+    ln -s .inputrc     ~/.inputrc
+    ln -s .vim         ~/.vim
+    ln -s .vimrc       ~/.vimrc
+    ln -s .zshrc       ~/.zshrc
+    ln -s .oh-my-zsh   ~/.oh-my-zsh
+    ln -s .tmux.conf   ~/.tmux.conf
+}
+
+main
