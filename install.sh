c6fa7d9e9cf0eaa711c7036a72c791ad32e072e3
Yousheng Ji <jasonyoushengji@gmail.com>
2022-08-29 17:09:20 +0800

    update ohmyzsh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ main() {
     ln -s "$(pwd)"/.vim         ~/.vim
     ln -s "$(pwd)"/.vimrc       ~/.vimrc
     ln -s "$(pwd)"/.zshrc       ~/.zshrc
-    ln -s "$(pwd)"/.oh-my-zsh   ~/.oh-my-zsh
+    ln -s "$(pwd)"/.ohmyzsh     ~/.ohmyzsh
     ln -s "$(pwd)"/.tmux.conf   ~/.tmux.conf
     chsh -s /bin/zsh
 }
